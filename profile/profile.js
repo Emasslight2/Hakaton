e5a9e0607d6ea44e10c882f0c13b0554aeb70da4
Emasslight2 <shabdan.asker@gmail.com>
2019-04-13 03:52:10 +0600

    eeeeeeeeeeeeeeeeeeee

--- profile/profile.js
+++ profile/profile.js
@@ -1,22 +1,8 @@
-// for(let i = 0; i < 11;i++){
-//     (function() {
-//         var elem = document.createElement("div");
-//             elem.innerHTML = "nice";
-//         var wrappedP = document.getElementById("wrapped");
-
-        
-//             wrappedP.appendChild(elem);
-//         console.log(elem);
-//     })();
-// }
-
-
 let users = [
     { nickname : 'Lord', level : 10, img : 'images/user.png', alt : 'nice', XP : 49 },
     { nickname : 'Baron', level : 50, img : 'images/userDog.jpg', alt : 'nice', XP : 100 },
     { nickname : 'King', level : 100, img : 'images/user2.jpg', alt : 'nice', XP : 87 }
 ];
-console.log(users[0].alt)
 
 var elem = document.getElementById('user-info');
 let d = new Date();
@@ -63,3 +49,65 @@ elem.innerHTML = `
       }
     }
   })();
+
+
+for(let i = 1; i <= users[2].level;i++){
+    (function() {
+        var elem = document.createElement("div");
+            elem.classList.add('icon');
+            elem.innerHTML = i;
+            if(i<=20) {
+                elem.style.background = 'url(levels/seed.png) top/cover';
+            }
+            else if(i<=40) {
+                elem.style.background = 'url(levels/seed2.png) top/cover';
+            }
+            else if(i<=60) {
+                elem.style.background = 'url(levels/plant.png) top/cover';
+            }
+            else if(i<=80) {
+                elem.style.background = 'url(levels/biggerplant.png) top/cover';
+            }
+            else {
+                elem.style.background = 'url(levels/tree.png) top/cover';
+            }
+            
+            if(i%10==1){
+                elem.style.border = '3px solid blue';
+            }
+            else if(i%10==2){
+                elem.style.border = '3px solid red';
+            }
+            else if(i%10==3){
+                elem.style.border = '3px solid brown';
+            }
+            else if(i%10==4){
+                elem.style.border = '3px solid lightblue';
+            }
+            else if(i%10==5){
+                elem.style.border = '3px solid black';
+            }
+            else if(i%10==6){
+                elem.style.border = '3px solid orange';
+            }
+            else if(i%10==7){
+                elem.style.border = '3px solid purple';
+            }
+            else if(i%10==8){
+                elem.style.border = '3px solid pink';
+            }
+            else if(i%10==9){
+                elem.style.border = '3px solid magenta';
+            }
+            else {
+                elem.style.border = '3px solid green';
+            }
+
+
+            console.log(i%10);
+        var icon = document.getElementById("levels-cntr");
+
+        
+            icon.appendChild(elem);
+    })();
+}
