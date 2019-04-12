a645b4edea6d410db352b0d4c7590e33c6c5d662
Emasslight2 <shabdan.asker@gmail.com>
2019-04-13 00:18:41 +0600

    js begin

--- profile/profile.js
+++ profile/profile.js
@@ -1,11 +1,22 @@
-for(let i = 0; i < 11;i++){
-    (function() {
-        var elem = document.createElement("div");
-            elem.innerHTML = "nice";
-        var wrappedP = document.getElementById("wrapped");
+// for(let i = 0; i < 11;i++){
+//     (function() {
+//         var elem = document.createElement("div");
+//             elem.innerHTML = "nice";
+//         var wrappedP = document.getElementById("wrapped");
 
         
-            wrappedP.appendChild(elem);
-        console.log(elem);
-    })();
-}
+//             wrappedP.appendChild(elem);
+//         console.log(elem);
+//     })();
+// }
+
+
+let users = [
+    {"nickname" : 'Lord', "level" : 10, "img" : 'user.png', "alt" : 'nice'}
+];
+console.log(users[0].alt)
+
+var elem = document.getElementById('user-info');
+let d = new Date();
+elem.innerHTML = `<img src="${users[0].img}" alt="${users[0].alt}" class="photo">`;
+
