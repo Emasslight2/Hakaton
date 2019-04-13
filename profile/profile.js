53fc96d39ea702c1b9c4327276b118a2d2c8df22
Emasslight2 <shabdan.asker@gmail.com>
2019-04-13 16:46:41 +0600

    nice

--- profile/profile.js
+++ profile/profile.js
@@ -1,6 +1,6 @@
 let users = [
     { nickname : 'Lord', level : 10, img : 'images/user.png', alt : 'nice', XP : 49 },
-    { nickname : 'Baron', level : 50, img : 'images/userDog.jpg', alt : 'nice', XP : 100 },
+    { nickname : 'Baron', level : 47, img : 'images/userDog.jpg', alt : 'nice', XP : 99 },
     { nickname : 'King', level : 100, img : 'images/user2.jpg', alt : 'nice', XP : 87 }
 ];
 
@@ -10,9 +10,9 @@ elem.innerHTML = `
 <article class="user-main">
     <div class="user-avatar">
         <div class="photo-cage">
-            <img src="${users[0].img}" alt="${users[0].alt}" class="photo">
+            <img src="${users[2].img}" alt="${users[2].alt}" class="photo">
         </div>
-        <h2>${users[0].nickname}</h2>
+        <h2>${users[2].nickname}</h2>
     </div>
     <div class="user-avatar">
         <div class="user-btn-cntr">
@@ -23,8 +23,8 @@ elem.innerHTML = `
 </article>
 <article class="user-additional">
     <section class="user-levels" id="level-words">
-        <span>Ваш уровень: ${users[0].level}</span>
-        <span>Следущий уровень: ${users[0].level+1}</span>
+        <span>Ваш уровень: ${users[2].level}</span>
+        <span>Следущий уровень: ${users[2].level+1}</span>
     </section>
     <section class="user-levels" id="progress-sec">
         <div id="myProgress">
@@ -32,7 +32,7 @@ elem.innerHTML = `
         </div>
     </section>
     <section class="user-levels" id="level-lower">
-        <span>${users[0].XP} XP</span>
+        <span>${users[2].XP} XP</span>
     </section>
 </article>`;
 
@@ -41,7 +41,7 @@ elem.innerHTML = `
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-      if (width >= users[0].XP) {
+      if (width >= users[2].XP) {
         clearInterval(id);
       } else {
         width++; 
@@ -51,7 +51,7 @@ elem.innerHTML = `
   })();
 
 
-for(let i = 1; i <= users[0].level;i++){
+for(let i = 1; i <= users[2].level;i++){
     (function() {
         var elem = document.createElement("div");
             elem.classList.add('icon');
@@ -73,38 +73,38 @@ for(let i = 1; i <= users[0].level;i++){
             }
             
             if(i%10==1){
-                elem.style.border = '3px solid blue';
+                elem.style.border = '4px solid rgb(119, 0, 0)';
             }
             else if(i%10==2){
-                elem.style.border = '3px solid red';
+                elem.style.border = '4px solid rgb(119, 45, 0)';
             }
             else if(i%10==3){
-                elem.style.border = '3px solid brown';
+                elem.style.border = '4px solid rgb(119, 91, 0)';
             }
             else if(i%10==4){
-                elem.style.border = '3px solid lightblue';
+                elem.style.border = '4px solid rgb(119, 119, 0)';
             }
             else if(i%10==5){
-                elem.style.border = '3px solid navy';
+                elem.style.border = '4px solid rgb(101, 119, 0)';
             }
             else if(i%10==6){
-                elem.style.border = '3px solid orange';
+                elem.style.border = '4px solid rgb(81, 119, 0)';
             }
             else if(i%10==7){
-                elem.style.border = '3px solid purple';
+                elem.style.border = '4px solid rgb(61, 119, 0)';
             }
             else if(i%10==8){
-                elem.style.border = '3px solid pink';
+                elem.style.border = '4px solid rgb(41, 119, 0)';
             }
             else if(i%10==9){
-                elem.style.border = '3px solid magenta';
+                elem.style.border = '4px solid rgb(29, 119, 0)';
             }
             else {
-                elem.style.border = '3px solid green';
+                elem.style.border = '4px solid rgb(0, 119, 9)';
+                elem.style.color = 'white';
             }
 
 
-            console.log(i%10);
         var icon = document.getElementById("levels-cntr");
 
         
